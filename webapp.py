5c5eb87a2f294c05ba1a9f43106ce228867f9e78
Melanie Campuzano <Undepth71@gmail.com>
2025-10-31 13:29:44 -0700

    Feat: avg of both genders for both SAT sections in data by states

--- webapp.py
+++ webapp.py
@@ -7,41 +7,50 @@ app = Flask(__name__)
 
 
 @app.route('/')
+def render_main():
 
+    return render_template('home.html')
 
 
 @app.route('/yearData1')
 def render_fact1():
     years = get_year_options()
-    # state = request.args.get('state')
-    # year = year_highest_average_math(years)
-    # fact1 = "In" + year + ", the state with the highest average math is " + state + "."
-    # year2 = year_highest_average_verbal(years)
-    # fact2 = "In" + year + ", the state with the highest average verbal is " + state + "."
-    # year3 = year_highest_average_GPA(years)
-    # fact3 = "In" + year + ", the state with the highest average GPA among all academic subjects is " + state "."
-    return render_template('page1.html', year_options = years) # funFactA=fact1, funFactB = fact2, funFactC = fact3, funfactD = fact4)
+    if "year" in request.args:
+        year = request.args.get('year')
+        state = request.args.get('state')
+        year1 = year_highest_average_math(year)
+        fact1 = "In " + year + ", the state with the highest average math is " + str(state) + "."
+        year2 = year_highest_average_verbal(year)
+        fact2 = "In " + year + ", the state with the highest average verbal is " + str(state) + "."
+        DataGPA = year_highest_average_GPA(year)
+        fact3 = "In " + year + ", the state with the highest average GPA among all academic subjects is " + DataGPA[0] + " and the GPA is " + str(DataGPA[1]) +  "."
+        # TotalTakers = total_test_takers_per_year(year)
+        # fact4 = "In " + year + ", the total amount of test takers was " + TotalTakers + "."
+        return render_template('page1.html', year_options = years, funFact1 = fact1, funFact2 = fact2, funFact3 = fact3)
+    return render_template('page1.html', year_options = years)
+
 
 @app.route('/yearData2')
 def render_fact2():
     states = get_state_options()
-    # years = request.args.net('year')
-    # state = final_total_avg_math()
-    # fact = "In" + state + ", the total average of math for all years is "+ ___ + "."
-    # state2 = final_total_avg_math_males()
-    # fact2 = "In" + state2 + ", the total average of verbal for all years is" + ____ + "."
-    # state3 = final_total_avg_math_females()
-    # fact3 = "In" + state3 + ", the total average of verbal for all years is" + ____ + "."
-    # state4 = final_total_avg_verbal()
-    # fact4 = "In" + state4 + ", the total average of verbal for all years is" + ____ + "."
-    # state5 = final_total_avg_verbal_males()
-    # fact5 = "In" + state5 + ", the total average of verbal for all years is" + ____ + "."
-    # state6 = final_total_avg_verbal_females()
-    # fact6 = "In" + state6 + ", the total average of verbal for all years is" + ____ + "."
-    # state7 = final_total_avg_GPA()
-    # fact7 = "In" + state7 + ", the total average GPA among all subjects for all years is" + ____ + "."
-
-    return render_template('page2.html', state_options = states) # funFactA = fact1, funFactB = fact2, funFactC = fact3, funfactD = fact4, funFactE = fact5, funFactF = fact6, funFactG = fact7)
+    if "state" in request.args:
+        state = request.args.get('state')
+        # state = final_total_avg_math()
+        # fact = "In" + state + ", the total average of math for all years is "+ ___ + "."
+        state2 = average_math_males(state)
+        fact2 = "In " + state + ", the total average of math for males for all years is " + str(state2) + "."
+        state3 = average_math_females(state)
+        fact3 = "In " + state + ", the total average of math for females for all years is " + str(state3) + "."
+        # state4 = final_total_avg_verbal()
+        # fact4 = "In" + state + ", the total average of verbal for all years is " + str(state4) + "."
+        state5 = average_verbal_males(state)
+        fact5 = "In " + state + ", the total average of verbal for males for all years is " + str(state5) + "."
+        state6 = average_verbal_females(state)
+        fact6 = "In " + state + ", the total average of verbal for females for all years is " + str(state6) + "."
+        # state7 = final_total_avg_GPA()
+        # fact7 = "In " + state + ", the total average GPA among all subjects for all years is" + str(state7) + "."
+        return render_template('page2.html', state_options = states, funFact2 = fact2, funFact3 = fact3, funFact5 = fact5, funFact6 = fact6) # funFactA = fact1, funfactD = fact4, funFactG = fact7)
+    return render_template('page2.html', state_options = states) 
 
 @app.route('/yearData3')
 def render_fact3():
@@ -68,7 +77,7 @@ def get_year_options():
         options += Markup("<option value=\"" + str(y) + "\">" + str(y) + "</option>") #Use Markup so <, >, " are not escaped lt, gt, etc.
     return options
 
-def get_state_options(): #Fix amount of states showing
+def get_state_options():
     with open('school_scores.json') as school_scores:
         data = json.load(school_scores)
     states=[]
@@ -81,45 +90,90 @@ def get_state_options(): #Fix amount of states showing
     return options
 
 
-def year_highest_average_math(year):
+def year_highest_average_math(year): # FIX THE SHOWING OF "NONE" AS RESULT
     """Return the name of a state in the given year with the highest average math score."""
     with open('school_scores.json') as school_scores:
         states = json.load(school_scores)
     highest=0
-    states = ""
+    state = ""
     for s in states:
-        if s["Years"] == year:
+        if s["Year"] == year:
             if s["Total"]["Math"] > highest:
                 highest = s["Total"]["Math"]
                 states = s["Year"]
     return year
 
-def year_highest_average_verbal(year):
+
+def year_highest_average_verbal(year): #FIX THE SHOWING OF "NONE" AS RESULT
     with open('school_scores.json') as school_scores:
         states = json.load(school_scores)
     highest=0
-    states = ""
+    state = ""
     for s in states:
-        if s["Years"] == year:
+        if s["Year"] == year:
             if s["Total"]["Verbal"] > highest:
                 highest = s["Total"]["Verbal"]
                 states = s["Year"]
     return year
 
-def year_highest_average_GPA(year): #work back on this
+def year_highest_average_GPA(year): #figure out  how to format decimals
     with open('school_scores.json') as school_scores:
         states = json.load(school_scores)
     highest=0
-    states = ""
+    state = ""
     for s in states:
-        if s["Year"] == year:
-            if s["Academic Subjects"] > highest:
-                highest = s["Academic Subjects"]
-                states = s["Year"]
-    return
+        if str(s["Year"]) == year:
+            average_GPA = avg_GPA(s["Academic Subjects"])
+            if average_GPA > highest:
+                highest = average_GPA
+                state = s["State"]["Name"]
+    return [state, highest]
     
+def avg_GPA(subjects):
+    total= 0
+    for subject, data in subjects.items():
+        print(subject)
+        total += data["Average GPA"]
+    return total/6
+
+def average_math_males(state):
+    with open('school_scores.json') as school_scores:
+        males_math = json.load(school_scores)
+        total = 0
+        for d in males_math:
+            if d['State']['Name'] == state:
+                total += d['Gender']['Male']['Math']
+    return total/10
+
+
+def average_math_females(state):
+    with open('school_scores.json') as school_scores:
+        females_math = json.load(school_scores)
+        total = 0
+        for d in females_math:
+            if d['State']['Name'] == state:
+                total += d['Gender']['Female']['Math']  
+    return total/10
     
 
+def average_verbal_males(state):
+    with open('school_scores.json') as school_scores:
+        males_verbal = json.load(school_scores)
+        total = 0
+        for d in males_verbal:
+            if d['State']['Name'] == state:
+                total += d['Gender']['Male']['Verbal']  
+    return total/10
+
+
+def average_verbal_females(state):
+    with open('school_scores.json') as school_scores:
+        females_verbal = json.load(school_scores)
+        total = 0
+        for d in females_verbal:
+            if d['State']['Name'] == state:
+                total += d['Gender']['Female']['Verbal']  
+    return total/10
 
 
 def avg_by_income_level(Income_level, section):
